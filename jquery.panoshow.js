2520111ed77fc4152d1760669c4f6984b4ea9344
Nirut Khemasakchai <rutcreate@gmail.com>
2014-05-14 23:50:03 +0700

    Basic jQuery plugin template

---
+++ jquery.panoshow.js
@@ -0,0 +1,38 @@
+;(function ($, window, document, undefined) {
+
+  'use strict';
+
+  var Panoshow = function(element, options) {
+    this.$element = $(element);
+    this.options = options;
+  };
+
+  Panoshow.prototype = {
+
+    init: function() {
+
+    },
+
+    _bindEvent: function() {
+
+    },
+
+    _buildElement: function() {
+
+    },
+
+    destroy: function() {
+
+    }
+
+  };
+
+  $.fn.panoShow = function(options) {
+
+    return this.each(function() {
+
+    });
+
+  };
+
+})(jQuery, this, this.document);
